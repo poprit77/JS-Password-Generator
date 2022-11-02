fdf7837576d55329908640ceb7fd67c523e69a3a
unknown <peterataras@gmail.com>
2022-11-02 15:09:28 -0700

    add upper and lowercase letter generator in JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment code here
 
 
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +17,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//generates a random number between 0 and 1 then multiples by 26 then rounds down and finally adds 97 to access lower case letters
+function getRandomlower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);  
+}
+//generates a random number between 0 and 1 then multiples by 26 then rounds down and finally adds 65 to access upper case letters
+
+function getRandomupper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);  
+}
+console.log(getRandomlower());
+
+console.log(getRandomupper());
