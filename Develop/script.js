369d6e7383d740b7690c407cc45453004bd745c0
unknown <peterataras@gmail.com>
2022-11-02 17:19:28 -0700

    JS structure

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,21 @@
-// Assignment code here
+// // Assignment code here
 
 
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// listens for click and runs function to grab length and values of check boxes
-//+ sign turns length into number
-//passes to generate password function
-generateBtn.addEventListener("click", () => {
-  const length = +lengthEL.value;
-  const hasLower = lowercaseEL.checked;
-  const hasUpper = uppercaseEL.checked;
-  const hasNumber = numberEL.checked;
-  const hassymbol = symbolsEL.checked;
 
-  resultEL.innerText = generatePassword(hasLower, hasNumber, hasUpper, hassymbol.length);
-});
 
 //Creates resultsfor HTMl to grab
 const resultEL = document.getElementById('result');
@@ -45,7 +34,64 @@ const randomFunc = {
   symbol: getRandomsymbol,
 }
 
-//generates a random number between 0 and 1 then multiples by 26 then rounds down and finally adds 97 to access lower case letters
+// listens for click and runs function to grab length and values of check boxes
+//+ sign turns length into number
+//passes to generate password function
+generate.addEventListener('click', () => {
+  const length = +lengthEL.value;
+  const hasLower = lowercaseEL.checked;
+  const hasUpper = uppercaseEL.checked;
+  const hasnumber = numberEL.checked;
+  const hassymbol = symbolsEL.checked;
+
+  resultEL.innerText = generatePassword(hasLower, hasnumber, hasUpper, hassymbol.length);
+});
+
+// copy to span
+
+// clipboardEL.addEventListener('click', () =>{
+//   const textarea = document.createElement('textarea');
+//   const password = resultEL.innerText;
+
+//   if (!password) {
+//     return;
+//   }
+
+//   textarea.value = password;
+//   document.body.appendChild(textarea);
+//   textarea.select();
+//   document.execCommand('copy');
+//   textarea.remove();
+//   alert('password copied to text!');
+// } );
+//
+function generatePassword(lower, upper, number, symbols, length) {
+
+  let generatePassword = '';
+
+  const typesCount = lower + upper + number + symbols;
+
+  const typesArr = [{ lower }, { upper }, { number }, { symbols }].filter
+    (
+      item => Object.values(item)[0]
+    );
+  if (typesCount === 0) {
+    return '';
+  }
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+
+      generatePassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
+//generates a  random number between 0 and 1 then multiples by 26 then rounds down and finally adds 97 to access lower case letters
 function getRandomlower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
